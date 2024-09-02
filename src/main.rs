7a4ed75f3f1cf1edf204c22e09fe9716aea46bae
Warren <42559252+wsmarshall@users.noreply.github.com>
2024-09-02 12:31:03 -0700

    debugs string formatting error

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 fn main() {
     let message = std::env::args()
-        .nth(1)
-        .expect("Missing the message. Usage: foxsay <message>");
-    println!(("{}"), message);
+        .nth(1) //needs expect since may call nonexistent index
+        .expect("Missing the message for use in foxsay!");
+    println!("{}", message);
     println!(" \\");
     println!("  \\");
     println!("     /\\_/\\");
     println!("    ( o o )");
-    println!("     ( I )");
+    println!("    -( I )-");
 }
