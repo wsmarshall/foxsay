9eefc67c9016fcda774ad832bbcc626ac526c78f
Warren <42559252+wsmarshall@users.noreply.github.com>
2024-09-02 12:27:34 -0700

    gets message from args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    let message = std::env::args()
+        .nth(1)
+        .expect("Missing the message. Usage: foxsay <message>");
+    println!(("{}"), message);
+    println!(" \\");
+    println!("  \\");
+    println!("     /\\_/\\");
+    println!("    ( o o )");
+    println!("     ( I )");
 }
