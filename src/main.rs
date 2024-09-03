ab5dc45bd3ce18bc267f8b385d3a13726abe6924
Warren <42559252+wsmarshall@users.noreply.github.com>
2024-09-03 13:21:56 -0700

    refines previous annotations

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,14 @@
 use clap::Parser;
 
 #[derive(Parser)]
-//this is command-line args def'n
+//[1] this is command-line args def'n
 struct Options {
     message: String,
 }
 
 fn main() {
-    //uses the derived Parser
-    let options = Options::parse(); //returns Options struct
-                                    //populated with parsed argument values
+    //[2] uses the derived Parser
+    let options = Options::parse(); //returns Options struct populated with parsed argument values
 
     let message = options.message;
     println!("{}", message);
