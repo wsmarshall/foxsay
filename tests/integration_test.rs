2150ce7f1ca3fae73d26d3e154383080ffff2d25
Warren <42559252+wsmarshall@users.noreply.github.com>
2024-09-07 13:45:38 -0700

    insts tests for what does the fox say, fails on dne file

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,4 +1,5 @@
 use assert_cmd::prelude::*; //imports some useful traits, like cargo_bin(), assert(), success()
+use predicates::prelude::*;
 use std::process::Command; //Command struct for running program in a newly spawned process
 
 #[test]
@@ -6,5 +7,16 @@ fn run_with_defaults() {
     Command::cargo_bin("foxsay")
         .expect("binary exists")
         .assert()
-        .success();
+        .success()
+        .stdout(predicate::str::contains("Wa-pa-pa-pa-pa-pa-pow!"));
+}
+
+#[test]
+fn fail_on_non_existing_file() -> Result<(), Box<dyn std::error::Error>> {
+    Command::cargo_bin("foxsay")
+        .expect("binary exists")
+        .args(&["-f", "no/such/file.txt"])
+        .assert()
+        .failure();
+    Ok(())
 }
